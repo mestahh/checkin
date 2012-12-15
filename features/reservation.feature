01af5ab71547b1baf7c83668a84a140a171e67d9
ebalmes <balazs.mester@gmail.com>
2012-12-15 08:35:48 +0100

    Feature definition for overlapping reservations. Not working yet.

--- features/reservation.feature
+++ features/reservation.feature
@@ -4,6 +4,16 @@ Feature: A user can reserve a time-slot for a service
     Given I have an existing user
 	And I am logged in
 	When I open a page of a certain shop
-	And I select a service and a timebox for that reservation
+	And I select a service and a start time for a reservation
 	Then a reserval is made in the database
 	And I should see my reservals
+	
+  Scenario: Unsuccessful reservation, time overlaps
+    Given I have an existing user
+	And I am logged in
+	And there is a service with a reservation
+	When I open a page of a certain shop
+	And I select the service and an overlapping start time for a reservation
+	Then a reserval should not be saved
+	And an error message should be shown
+	
--- features/step_definitions/reservation.rb
+++ features/step_definitions/reservation.rb
@@ -4,7 +4,7 @@ When /^I open a page of a certain shop$/ do
   visit shop_path(@shop)
 end
 
-When /^I select a service and a timebox for that reservation$/ do
+When /^I select a service and a start time for a reservation$/ do
   select 'ServiceName', {:from => 'service'}
   click_button 'Next'
   fill_in 'start_time', :with => '2012-12-12 12:12:12'
@@ -21,4 +21,27 @@ end
 
 Then /^I should see my reservals$/ do
   current_path.should == reservations_path
+end
+
+Given /^there is a service with a reservation$/ do
+  @reservation = Reservation.new
+  @reservation.service_id = 1
+  @reservation.user_id = 1
+  @reservation.start_time = '2012-12-12 12:00:00'
+  @reservation.save
+end
+
+When /^I select the service and an overlapping start time for a reservation$/ do
+  select 'ServiceName', {:from => 'service'}
+  click_button 'Next'
+  fill_in 'start_time', :with => '2012-12-12 12:12:12'
+  click_button 'Reserve'
+end
+
+Then /^a reserval should not be saved$/ do
+  Reservation.count.should == 2
+end
+
+Then /^an error message should be shown$/ do
+  pending # express the regexp above with the code you wish you had
 end
